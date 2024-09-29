fc9a2cff75256ef90f3d75e4a44a3558e8516225
Shivaay09 <135844761+Shivaay09@users.noreply.github.com>
2024-09-29 20:23:06 +0000

    variables and datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat all JS code as newer version
+
+// alert("hello"); // we are using nodejs, not browser
+
+let name = "Shivam";
+let age = 20;
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null);
