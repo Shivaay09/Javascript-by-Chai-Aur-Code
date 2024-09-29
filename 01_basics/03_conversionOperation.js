fd65a839a53cb4c1f94cf32c687399aa60e4299d
Shivaay09 <135844761+Shivaay09@users.noreply.github.com>
2024-09-29 20:56:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score = "33";
+console.log(typeof score);
+
+let numberType = Number(score);
+console.log(typeof numberType)
+
+let age = 21;
+console.log(typeof age);
+
+
+score = true;
+console.log(typeof score);
+console.log(score);
+console.log(Number(score));
+
+score = "44ads";
+console.log(typeof score);
+console.log(score);
+console.log(Number(score));
+
+score = null;
+console.log(typeof score);
+console.log(score);
+console.log(Number(score));
+
+score = 13;
+console.log(typeof score);
+console.log(score);
+console.log(Boolean(score));
+
+score = 13;
+console.log(typeof score);
+console.log(score);
+console.log(typeof String(score));
+console.log(String(score));
+
+
